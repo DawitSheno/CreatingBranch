a3443a335a4cb3167ec7ddd25ac261ad2673b590
dba <dba@DAWIT-QA.wireless.public.local>
2019-11-17 15:38:22 -0500

    This class has been added

---
+++ CreatingBranch/src/SimplePrint.java
@@ -0,0 +1,19 @@
+
+public class SimplePrint {
+
+	private static CharSequence Y;
+
+	public static void main(String[] args) {
+
+		
+		String Str = "Yes";
+		if(Str.contains("Yes sir")) {
+			System.out.println(" we are correct");
+			
+		}else {
+			System.out.println("we are Not Correct ");
+			
+		}
+	}
+
+}
